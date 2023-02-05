e06df695b70dd892bf8771f3960de0fa280ad70c
mscottreynolds <rey22006@byui.edu>
2023-02-04 19:31:17 -0700

    Menu, section, date updates for base style.

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,13 +1,25 @@
-// Current date
-const d = new Date();
+// Current date, Day of week, date month year format, or UK format.
+const now = new Date();
 const current_date = document.querySelector("#current-date");
-current_date.innerHTML = d;
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+    dateStyle: "full",
+}).format(now);
+current_date.innerHTML = fulldateUK;
 
 // Set the year for the copyright.
 const copyright_year = document.querySelector("#copyright-year");
-const year = d.getFullYear();
+const year = now.getFullYear();
 copyright_year.innerHTML = `${year}`;
 
 // Set the last modified date.
 const last_modified = document.querySelector("#last-modified");
 last_modified.innerHTML = `${document.lastModified}`;
+
+const toggleMenu = function () {
+    console.log("It worked!");
+    document.querySelector("#primaryNav").classList.toggle("open");
+    document.querySelector("#hamburgerBtn").classList.toggle("open");
+};
+
+const hamburgerBtn = document.getElementById("hamburgerBtn");
+hamburgerBtn.onclick = toggleMenu;
