eb55fe276b64707663fdfb56ce2fb9c1635e66a7
mscottreynolds <rey22006@byui.edu>
2023-03-18 21:11:31 -0600

    Clean up script and change the way two random cards are picked.

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -18,89 +18,86 @@ const membershipToText = (level) => {
 };
 
 const displaySpotlight = (directory) => {
-    // console.log(directory);
-    // console.log("table follows:");
-    // console.table(directory);
-
     const cards = document.querySelector("#spotlight div.cards"); // select the output container element
 
-    let spotlightCount = 0;
-    const filtered = directory
-        .filter((item) => {
-            return (item.membershipLevel == "silver" || item.membershipLevel == "gold");
-        });
-    // console.log(`filtered = ${filtered}`);
-    // console.table(filtered);
-    filtered.forEach(
-            (entry) => {
-                const random = Math.random() >= 0.5;
-                if (random && spotlightCount < 2) {
-                    // Display two random records to spotlight.
-                    spotlightCount += 1;
-                    // console.log(entry);
-                    // Create elements to add to the div.cards element
-                    const card = document.createElement("section");
-                    const name = document.createElement("h3");
-                    const description = document.createElement("p");
-                    const picture = document.createElement("img");
-                    picture.setAttribute("class", "box-shadow");
-                    const infoList = document.createElement("ul");
-                    const address = document.createElement("li");
-                    const cityStateZip = document.createElement("li");
-                    const phone = document.createElement("li");
-                    const href = document.createElement("a");
-                    // const membershipLevel = document.createElement("li");
-
-                    // "name": "Worship Services",
-                    // "address": "124 Legacy Ranch Ln.",
-                    // "city": "Legacy Ranch",
-                    // "state": "UT",
-                    // "zip": "84000",
-                    // "phone": "234-555-6790",
-                    // "url": "https://worship.legacyranch.group",
-                    // "imageurl": "images/worship.webp",
-                    // "membershipLevel": "np",
-                    // "description": "Come worship with us."
-
-                    name.textContent = `${entry.name}`;
-                    description.textContent = `${entry.description}`;
-                    picture.setAttribute("src", entry.imageurl);
-                    picture.setAttribute("alt", entry.description);
-                    picture.setAttribute("loading", "lazy");
-                    picture.setAttribute("width", "100");
-                    address.textContent = `${entry.address}`;
-                    cityStateZip.textContent = `${entry.city}, ${entry.state}  ${entry.zip}`;
-                    phone.textContent = `${entry.phone}`;
-                    // membershipLevel.textContent = `${membershipToText(
-                    //     entry.membershipLevel
-                    // )}`;
-
-                    href.setAttribute("href", entry.url);
-                    href.textContent = `Website`;
-                    href.setAttribute("target", "_blank");
-                    // url.appendChild(href);
-
-                    // infoList.appendChild(description);
-                    infoList.appendChild(address);
-                    infoList.appendChild(cityStateZip);
-                    infoList.appendChild(phone);
-                    // infoList.appendChild(membershipLevel);
-                    // infoList.appendChild(url);
-
-                    // Append the section(card) with the created elements
-                    card.appendChild(name);
-                    card.appendChild(picture);
-                    card.appendChild(description);
-                    card.appendChild(infoList);
-                    // card.appendChild(address);
-                    // card.appendChild(cityStateZip);
-                    card.appendChild(href);
-
-                    cards.appendChild(card);
-                }
-            
-            } // end of forEach loop
+    const filtered = directory.filter((item) => {
+        return (
+            item.membershipLevel == "silver" || item.membershipLevel == "gold"
         );
+    });
+
+    // Grab a couple random cards.
+    const randomCards = [];
+    while (randomCards.length < 2 && filtered.length > 0) {
+        const randomCard = Math.floor(Math.random() * filtered.length);
+        randomCards.push(filtered[randomCard]);
+        filtered.splice(randomCard, 1);
+    }
+
+    randomCards.forEach(
+        (entry) => {
+            // Create elements to add to the div.cards element
+            const card = document.createElement("section");
+            const name = document.createElement("h3");
+            const description = document.createElement("p");
+            const picture = document.createElement("img");
+            picture.setAttribute("class", "box-shadow");
+            const infoList = document.createElement("ul");
+            const address = document.createElement("li");
+            const cityStateZip = document.createElement("li");
+            const phone = document.createElement("li");
+            const href = document.createElement("a");
+            // const membershipLevel = document.createElement("li");
+
+            // "name": "Worship Services",
+            // "address": "124 Legacy Ranch Ln.",
+            // "city": "Legacy Ranch",
+            // "state": "UT",
+            // "zip": "84000",
+            // "phone": "234-555-6790",
+            // "url": "https://worship.legacyranch.group",
+            // "imageurl": "images/worship.webp",
+            // "membershipLevel": "np",
+            // "description": "Come worship with us."
+
+            name.textContent = `${entry.name}`;
+            description.textContent = `${entry.description}`;
+            picture.setAttribute("src", entry.imageurl);
+            picture.setAttribute("alt", entry.description);
+            picture.setAttribute("loading", "lazy");
+            picture.setAttribute("width", "100");
+            address.textContent = `${entry.address}`;
+            cityStateZip.textContent = `${entry.city}, ${entry.state}  ${entry.zip}`;
+            phone.textContent = `${entry.phone}`;
+            // membershipLevel.textContent = `${membershipToText(
+            //     entry.membershipLevel
+            // )}`;
+
+            href.setAttribute("href", entry.url);
+            href.textContent = `Website`;
+            href.setAttribute("target", "_blank");
+            // url.appendChild(href);
+
+            // infoList.appendChild(description);
+            infoList.appendChild(address);
+            infoList.appendChild(cityStateZip);
+            infoList.appendChild(phone);
+            // infoList.appendChild(membershipLevel);
+            // infoList.appendChild(url);
+
+            // Append the section(card) with the created elements
+            card.appendChild(name);
+            card.appendChild(picture);
+            card.appendChild(description);
+            card.appendChild(infoList);
+            // card.appendChild(address);
+            // card.appendChild(cityStateZip);
+            card.appendChild(href);
+
+            cards.appendChild(card);
+            // }
+        } // end of forEach loop
+    );
 }; // end of function expression
 
 async function getDirectoryData() {
